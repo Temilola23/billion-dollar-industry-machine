90298c9cf89242e142559ed0cb2765524c2eae42
Temilola Olowolayemo <133580838+Temilola23@users.noreply.github.com>
2025-10-25 05:13:10 -0500

    Initial commit: Generated by OPS-X
    Build a web application called 'Billion dollar industry machine'. Pages needed: Home, Dashboard Requirements: I want an app that generates me something sleek for a billion dollar industry machine

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import IndustryGenerator from '@/components/IndustryGenerator';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <IndustryGenerator />
+    </main>
+  );
+}
